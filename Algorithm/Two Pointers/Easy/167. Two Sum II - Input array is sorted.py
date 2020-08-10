3ab9e015f3887881a9786c13188f172f20fd720f
BethZeng <47905122+BethZeng@users.noreply.github.com>
2020-08-10 19:05:49 -0400

    Update 167. Two Sum II - Input array is sorted.py

--- Algorithm/Two Pointers/Easy/167. Two Sum II - Input array is sorted.py
+++ Algorithm/Two Pointers/Easy/167. Two Sum II - Input array is sorted.py
@@ -1,4 +1,4 @@
-## 167. Two Sum II - Input array is sorted 
+"""
 Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
 
 The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
@@ -15,6 +15,9 @@ Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
 ```
 
 There are three solutions: dictionary, two - pointers, and binary search
+“”“
+
+Solutions:
 
 # dictionary
 
