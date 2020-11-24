c9626a94dd43046d58a8d60da23233b8d807992e
BethZeng <47905122+BethZeng@users.noreply.github.com>
2020-11-24 11:51:55 +0800

    Create 04. 二维数组中的查找.py

---
+++ 剑指Offer/python/04. 二维数组中的查找.py
@@ -0,0 +1,15 @@
+"""
+在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数
+"""
+
+class Solution:
+    def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+
+        i, j = len(matrix) - 1, 0
+        while i >= 0 and j < len(matrix[0]):
+            if matrix[i][j] > target: i -= 1
+            elif matrix[i][j] < target: j += 1
+            else: return True
+        return False
